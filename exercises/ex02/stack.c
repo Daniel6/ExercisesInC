a4517dc6a937e9a9069cef788da3eb5ad636b1bb
dbishop <dbishop.rwc@gmail.com>
2017-01-25 15:02:21 -0500

    exercise 2 done

--- exercises/ex02/stack.c
+++ exercises/ex02/stack.c
@@ -12,23 +12,27 @@ License: GNU GPLv3
 
 int *foo() {
     int i;
-    int array[SIZE];
 
-    //printf("%p\n", array);
+    // Note: as discussed below, a possible fix would be to malloc the array as such:
+    int *array = malloc(SIZE);
+    // int array[SIZE]; // create a local array
 
-    for (i=0; i<SIZE; i++) {
+    printf("%p\n", array); // display the memory address of this local array
+
+    for (i=0; i<SIZE; i++) { // set all elements of this local array to 42
 	array[i] = 42;
     }
-    return array;
+    return array; // return the memory address of local array
 }
 
 void bar() {
     int i;
-    int array[SIZE];
+    int array[SIZE]; // create a local array
 
-    //printf("%p\n", array);
+    // display the memory address of this local array. It is the same as the address of the local array in foo()
+    printf("%p\n", array); 
 
-    for (i=0; i<SIZE; i++) {
+    for (i=0; i<SIZE; i++) { // set the elements of this array to 0, 1, 2, ...
 	array[i] = i;
     }
 }
@@ -36,12 +40,19 @@ void bar() {
 int main()
 {
     int i;
-    int *array = foo();
-    bar();
+    int *array = foo(); // create a pointer pointing at the memory address of the local array created in foo()
+    bar(); // create and set elements of local array in bar()
 
+    // One would expect that this would print 42 repeatedly, however, since array points to a local variable
+    // in a function that has exited, the array has long since been cleaned up. Thus by reading the data that
+    // is now at the address where the array used to be, we have undefined behavior.
     for (i=0; i<SIZE; i++) {
-	printf("%d\n", array[i]);
+	   printf("%d\n", array[i]);
     }
+
+    // If using malloc to prevent this issue, one must also remember to free the resources allocated or risk
+    // memory leakage
+    free(array);
 
     return 0;
 }
